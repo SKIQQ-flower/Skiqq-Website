2c04742540d8a852f32907658e33175204dce795
SKIQQ <119549330+SKIQQ-flower@users.noreply.github.com>
2023-11-27 19:17:50 -0300

    meta tags!

--- app.vue
+++ app.vue
@@ -8,17 +8,20 @@ const head = useLocaleHead({
 
 
 useHead({
-    titleTemplate: (titleChunk) => {
-        return titleChunk ? `${t(titleChunk)} - ${t('website_name')}` : `${t('website_name')}`;
-    },
-    link: [
-        {
-          rel: 'icon',
-          type: 'image/x-icon',
-          href: '/svg/bodedope icon.svg'
-        }
-    ],
-    
+  titleTemplate: (titleChunk) => {
+      return titleChunk ? `${t(titleChunk)} - ${t('website_name')}` : `${t('website_name')}`;
+  },
+  link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/svg/bodedope icon.svg'
+      }
+  ],
+  meta: [
+    { name: 'description', content: 'O bode mais fofo do universo!' },
+    { name: 'keywords', content: 'vue, nuxt, javascript, autismo, autism, skiqq' }
+  ],
 })
 </script>
 
