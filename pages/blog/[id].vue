3d110761c14b48058f76efcabdcc8fc1bda41a22
SKIQQ <chibifoxdiscord+github@gmail.com>
2023-12-09 13:15:47 -0300

    im noob

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -2,6 +2,21 @@
 import { DateTime } from "luxon";
 const route = useRoute()
 const { data } = await useAsyncData('home', () => queryContent(route.params.id).findOne())
+
+if (data && data.value) {
+    useSeoMeta({
+        ogTitle: data.value.title,
+        description: data.value.description,
+        ogDescription: data.value.description,
+        ogImage: {
+            url: data.value.cover,
+            width: '1200'
+        },
+        twitterCard: "summary_large_image",
+        author: data.value.author,
+    })
+}
+
 const { locale } = useI18n()
 const { t } = useI18n()
 useContentHead(data)
@@ -10,30 +25,23 @@ useHead({
       return titleChunk ? `${t(titleChunk)} - ${t('blog_name')}` : `${t('blog_name')}`;
   },
 })
-useSeoMeta({
-  ogTitle: data.value.title,
-  description: data.value.description,
-  ogDescription: data.value.description,
-  ogImage: data.value.cover,
-  twitterCard: data.value.cover,
-  author: data.value.author,
-})
 const popupActive = ref(true)
 
 </script>
 
 <template>
     <popup @close="popupActive = !popupActive" title="" type="login" :visible="popupActive" />
-    <div class="center">
+    <div v-if="data" class="center">
         <NuxtImg class="blog-cover" :src="data.cover"/>
         <h2 class="blog-title">{{ data.title }}</h2>
-        
-        <ContentRenderer class="card-vertical blog" :value="data" />
+
+        <ContentRenderer class="card-vertical blog" :value="data"/>
         <div class="blogfooter">
             <NuxtImg style="border-radius: 50%;" :src="data.authorpfp" width="40"/>
             <p><strong>{{ data.author }}</strong> {{ $t('at_time_blog') }} {{ DateTime.fromISO(data.date).setLocale(locale).toLocaleString(DateTime.DATETIME_MED) }}</p>
         </div>
     </div>
+    <h1 v-else>Sinas</h1>
 </template>
   
 
