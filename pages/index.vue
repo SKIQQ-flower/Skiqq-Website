2a7148d387f9b8477138a76a70a28cc06b76a1a2
Skiqq <chibifoxdiscord@gmail.com>
2024-01-04 15:10:03 -0300

    Adjust the position of the gradient blur elements.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col items-center my-5 gap-5">
     <div class="relative flex flex-col items-center w-full">
       <div class="bg-gradient-radial absolute z-[-1] left-[-5rem] top-[-10rem] w-[20rem] h-[20rem] dark:from-purple-900
       from-purple-500 blur-2xl"></div>
@@ -15,7 +15,7 @@
     <div class="relative flex flex-col items-center w-full">
       <div class="bg-gradient-radial absolute z-[-1] left-[-5rem] top-[9rem] w-[20rem] h-[20rem] dark:from-purple-900
       from-purple-500 blur-2xl"></div>
-      <div class="bg-gradient-radial absolute z-[-1] right-[0rem] bottom-[-5rem] w-[20rem] h-[20rem] from-yellow-300 dark:from-yellow-900 blur-2xl"></div>
+      <div class="bg-gradient-radial absolute z-[-1] right-[0rem] bottom-[-10rem] w-[20rem] h-[20rem] from-yellow-300 dark:from-yellow-900 blur-2xl"></div>
       <BirthdayCard/>
       <SocialMedia/>
       <div class="bg-gradient-radial absolute z-[-1] right-[0rem] bottom-[-10rem] w-[20rem] h-[20rem] sm:from-transparent from-purple-500 dark:from-purple-900 blur-2xl"></div>
