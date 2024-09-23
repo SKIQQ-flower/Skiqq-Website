eb06fa50ee7b612fe78e8d0864742c6da436744a
SKIQQ <119549330+SKIQQ-flower@users.noreply.github.com>
2024-09-23 14:43:56 +0000

    og image workaround

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2024-09-11',
   devtools: { enabled: true },
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxt/icon",
     "@nuxt/content",
     "@nuxtjs/seo",
-    '@nuxt/image'
+    '@nuxt/image',
   ],
   site: {
     url: 'https://skiqq.pw',
     name: 'Skiqq Website',
     description: 'The website of the cutest goat of the world!',
     defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
-  }
+  },
+  build: {
+    transpile: ['shiki'],
+  },
 })
