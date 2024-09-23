3d86b1364215195097c12330afb30f3bff57c2e4
SKIQQ <119549330+SKIQQ-flower@users.noreply.github.com>
2024-09-23 15:16:40 +0000

    merge things

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,77 +2,23 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: [
-    '@nuxtjs/color-mode',
-    'nuxt-icon',
-    '@nuxtjs/i18n',
+    '@nuxt/icon',
+    "@nuxt/content",
+    "@nuxtjs/seo",
     '@nuxt/image',
-    '@vueuse/nuxt',
-    '@nuxt/content',
   ],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  colorMode: {
-    classSuffix: '',
-  },
-  i18n: {
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieKey: 'i18n_redirected',
-      redirectOn: 'root',  // recommended
-    },
-    baseUrl: 'https://skiqq.vercel.app',
-    strategy: 'prefix_except_default',
-    defaultLocale: 'en-US',
-    locales: [
-      {
-        code: 'en-US',
-        iso: 'en-US',
-        isCatchallLocale: true
-      }, 
-      {
-        code: 'pt-BR',
-        iso: 'pt-BR',
-        isCatchallLocale: true
-      }
-    ],
-    vueI18n: './i18n.config.ts' // if you are using custom path, default 
-  },
-  image: {
-      domains: [
-        'avatars0.githubusercontent.com'
-      ]
+  site: {
+    url: 'https://skiqq.pw',
+    name: 'Skiqq Website',
+    description: 'The website of the cutest goat of the world!',
+    defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
   },
   routeRules: {
     '/vita-capris': { redirect: 'https://discord.gg/QhQkJhfEMZ' },
     '/netty': { redirect: 'https://discord.gg/C9x5ZVy2YR' },
     '/fdg': { redirect: 'https://discord.gg/faixa-de-gaza-1238423927181803521' },
   },
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      link: [
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com'
-        },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans',
-          crossorigin: ''
-        }
-      ]
-    }
+  build: {
+    transpile: ['shiki'],
   },
-  css: [
-    '@/assets/css/style.css',
-  ],
 })
